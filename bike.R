3a322637e8864ba27262d771663b149b0801a237
kimmujeong <anwjd7@gmail.com>
2019-03-09 15:35:18 +0900

    bikte test3

--- bike.R
+++ bike.R
@@ -86,5 +86,3 @@ head(test)
 #write.csv자체 함수에서 이용할수없는거같고 따라서 write.table함수를 이용해야함
 #write.csv(test[,c("datetime","count2")],file="result2.csv",row.names = FALSE,col.names = c("datetime","count")) 오류코드
 write.table(test[,c("datetime","count2")],file="result2.csv",row.names = FALSE,col.names = c("datetime","count"),sep=",")
-
-#수정
