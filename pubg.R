d5c261f5175cae3f8d8a80c6e55e5767d15b663e
kimmujeong <anwjd7@gmail.com>
2019-03-12 19:34:20 +0900

    pubg xgboost update

--- pubg.R
+++ pubg.R
@@ -1,8 +1,8 @@
 library(dplyr)
 library(ggplot2)
-
-pubg_train<-read.csv("C:\\Users\\anwjd\\Downloads\\pubg-finish-placement-prediction\\train_V2.csv")
-test<-read.csv("C:\\Users\\anwjd\\Downloads\\pubg-finish-placement-prediction\\test_V2.csv")
+library(xgboost)
+pubg_train<-read.csv("C:\\Users\\thgus\\Downloads\\pubg-finish-placement-prediction\\train_V2.csv")
+pubg_test<-read.csv("C:\\Users\\thgus\\Downloads\\pubg-finish-placement-prediction\\test_V2.csv")
 save(pubg_train,file="./pubg_train.RData")
 
 head(pubg_train,20)
@@ -13,9 +13,9 @@ nrow(pubg_train)
 pubg_train %>%
   count(pubg_train$groupId)
 
-pubg_train$groupId
-pubg_train[!duplicated(pubg_train$groupId),]
-distinct(pubg_train,groupId)
+#pubg_train$groupId
+#pubg_train[!duplicated(pubg_train$groupId),]
+#distinct(pubg_train,groupId)
 
 # rankPoints -1의 경우 None, 제외
 # rankPoints의 값이 -1이 아닌 경우(rankPoints의 의미있는 경우) -> killPoints가 0이라면 None, 제외
@@ -49,74 +49,103 @@ head(new_pubg_train[order(new_pubg_train$winPoints,decreasing = TRUE),])
 cor(new_pubg_train$killPoints, new_pubg_train$winPlacePerc) #0.0898857
 cor(new_pubg_train$winPoints, new_pubg_train$winPlacePerc) #0.2046172
 
-###클러스터링
-###kmeans 클러스터링의 경우 평균값을 이용하기 때문에 컬럼은 모두 숫자로 구성되어 있어야함
-###data에 NA값 없어야함
-cluster.train.data<-subset(new_pubg_train,select = -c(Id,groupId,matchId,matchType))
-str(cluster.train.data)
+############################################################################################
+############################################################################################
+############################################################################################
+# ###클러스터링
+# ###kmeans 클러스터링의 경우 평균값을 이용하기 때문에 컬럼은 모두 숫자로 구성되어 있어야함
+# ###data에 NA값 없어야함
+# cluster.train.data<-subset(new_pubg_train,select = -c(Id,groupId,matchId,matchType))
+# str(cluster.train.data)
+# 
+# #numeric 작업(클러스터링때문에-scale작업위해서), 
+# cluster.train.data$assists<-as.numeric(cluster.train.data$assists)
+# cluster.train.data$boosts<-as.numeric(cluster.train.data$boosts)
+# cluster.train.data$DBNOs<-as.numeric(cluster.train.data$DBNOs)
+# cluster.train.data$headshotKills<-as.numeric(cluster.train.data$headshotKills)
+# cluster.train.data$heals<-as.numeric(cluster.train.data$heals)
+# cluster.train.data$killPlace<-as.numeric(cluster.train.data$killPlace)
+# cluster.train.data$killPoints<-as.numeric(cluster.train.data$killPoints)
+# cluster.train.data$kills<-as.numeric(cluster.train.data$kills)
+# cluster.train.data$killStreaks<-as.numeric(cluster.train.data$killStreaks)
+# cluster.train.data$matchDuration<-as.numeric(cluster.train.data$matchDuration)
+# cluster.train.data$maxPlace<-as.numeric(cluster.train.data$maxPlace)
+# cluster.train.data$numGroups<-as.numeric(cluster.train.data$numGroups)
+# cluster.train.data$rankPoints<-as.numeric(cluster.train.data$rankPoints)
+# cluster.train.data$revives<-as.numeric(cluster.train.data$revives)
+# cluster.train.data$roadKills<-as.numeric(cluster.train.data$roadKills)
+# cluster.train.data$teamKills<-as.numeric(cluster.train.data$teamKills)
+# cluster.train.data$vehicleDestroys<-as.numeric(cluster.train.data$vehicleDestroys)
+# cluster.train.data$weaponsAcquired<-as.numeric(cluster.train.data$weaponsAcquired)
+# cluster.train.data$winPoints<-as.numeric(cluster.train.data$winPoints)
+# str(cluster.train.data)
+# head(cluster.train.data)
+# 
+# scaled_data<-scale(cluster.train.data)
+# is.na(scaled_data)
+# sum(is.na(scaled_data)) #결측치몇개있는지 
+# scaled_data<-subset(scaled_data,select = -c(rankPoints)) #NA값인 rankpoints 제거
+# head(scaled_data)
+# 
 
-#numeric 작업(클러스터링때문에-scale작업위해서), 
-cluster.train.data$assists<-as.numeric(cluster.train.data$assists)
-cluster.train.data$boosts<-as.numeric(cluster.train.data$boosts)
-cluster.train.data$DBNOs<-as.numeric(cluster.train.data$DBNOs)
-cluster.train.data$headshotKills<-as.numeric(cluster.train.data$headshotKills)
-cluster.train.data$heals<-as.numeric(cluster.train.data$heals)
-cluster.train.data$killPlace<-as.numeric(cluster.train.data$killPlace)
-cluster.train.data$killPoints<-as.numeric(cluster.train.data$killPoints)
-cluster.train.data$kills<-as.numeric(cluster.train.data$kills)
-cluster.train.data$killStreaks<-as.numeric(cluster.train.data$killStreaks)
-cluster.train.data$matchDuration<-as.numeric(cluster.train.data$matchDuration)
-cluster.train.data$maxPlace<-as.numeric(cluster.train.data$maxPlace)
-cluster.train.data$numGroups<-as.numeric(cluster.train.data$numGroups)
-cluster.train.data$rankPoints<-as.numeric(cluster.train.data$rankPoints)
-cluster.train.data$revives<-as.numeric(cluster.train.data$revives)
-cluster.train.data$roadKills<-as.numeric(cluster.train.data$roadKills)
-cluster.train.data$teamKills<-as.numeric(cluster.train.data$teamKills)
-cluster.train.data$vehicleDestroys<-as.numeric(cluster.train.data$vehicleDestroys)
-cluster.train.data$weaponsAcquired<-as.numeric(cluster.train.data$weaponsAcquired)
-cluster.train.data$winPoints<-as.numeric(cluster.train.data$winPoints)
-str(cluster.train.data)
-head(cluster.train.data)
+# #############클러스터 k 값 찾는 과정
+# set.seed(109)
+# wss<-0
+# for (j in 1:30) {
+#   km.out <- kmeans(scaled_data, centers = j, nstart = 20)
+#   wss[j] <- km.out$tot.withinss
+# }
+# 
+# wss_df<-data.frame(num_cluster=1:14,wss=wss)
+# ggplot(data = wss_df, aes(x=num_cluster, y= wss))+geom_line(color = "lightgrey", size = 2) +
+#   geom_point(color = "green", size = 4)
+# 
+# #클러스터 감소추이를 알아보기 위해서. 감소폭이 더이상 작아지지않을떄가 최적. 11
+# for(i in 1:14){
+#   wss_df$gap[i]<-wss_df$wss[i]-wss_df$wss[i+1]
+# }
+# wss_df
+# ###########################################################################################
+# 
+# k<-11
+# kmeans.model<-kmeans(scaled_data,centers = k,nstart=20) #nstart:시행횟수
+# 
+# cluster.train.data$cluster<-kmeans.model$cluster
+# 
+# table(cluster.train.data$winPlacePerc,kmeans.model$cluster)
+# 
+# cluster.train.data$winPlacePerc
+# qplot(killPoints,winPlacePerc,colour=cluster,data=cluster.train.data)
+############################################################################################
+############################################################################################
+############################################################################################
 
-scaled_data<-scale(cluster.train.data)
-is.na(scaled_data)
-sum(is.na(scaled_data)) #결측치몇개있는지 
-scaled_data<-subset(scaled_data,select = -c(rankPoints)) #NA값인 rankpoints 제거
-head(scaled_data)
+############################################################################################
+############################################################################################
+############################################################################################
+###XGBOOST
 
-###########################################################################################
-#############클러스터 k 값 찾는 과정
-set.seed(109)
-wss<-0
-for (j in 1:30) {
-  km.out <- kmeans(scaled_data, centers = j, nstart = 20)
-  wss[j] <- km.out$tot.withinss
-}
+#install.packages("xgboost")
+colnames(new_pubg_train)
+xgboost_pubg_train<-data.matrix(subset(new_pubg_train,select = -c(Id,groupId,matchId,matchType,winPlacePerc)))
+xgb<-xgboost(data=xgboost_pubg_train,label=new_pubg_train$winPlacePerc,
+             eta = 0.2,
+             nround = 600,
+             subsample = 0.8,
+             colsample_bytree = 0.8,
+             seed = 1,
+             eval_metric = "mae",
+             objective = "reg:linear",
+             nthread = 3,
+             max_depth = 5)
 
-wss_df<-data.frame(num_cluster=1:14,wss=wss)
-ggplot(data = wss_df, aes(x=num_cluster, y= wss))+geom_line(color = "lightgrey", size = 2) +
-  geom_point(color = "green", size = 4)
+xgboost_pubg_test<-data.matrix(subset(pubg_test,select = -c(Id,groupId,matchId,matchType)))
+xgb_pred<-predict(xgb,xgboost_pubg_test)
+head(xgb_pred)
+head(pubg_test)
+pubg_test$winPlacePerc<-xgb_pred
+head(pubg_test)
+write.csv(pubg_test[,c("Id","winPlacePerc")],file="submission.csv",row.names = FALSE)
 
-#클러스터 감소추이를 알아보기 위해서. 감소폭이 더이상 작아지지않을떄가 최적. 11
-for(i in 1:14){
-  wss_df$gap[i]<-wss_df$wss[i]-wss_df$wss[i+1]
-}
-wss_df
-###########################################################################################
-
-k<-11
-kmeans.model<-kmeans(scaled_data,centers = k,nstart=20) #nstart:시행횟수
-
-cluster.train.data$cluster<-kmeans.model$cluster
-
-table(cluster.train.data$winPlacePerc,kmeans.model$cluster)
-
-cluster.train.data$winPlacePerc
-qplot(killPoints,winPlacePerc,colour=cluster,data=cluster.train.data)
-
-
-
-
-
-
+#write.table(pubg_test[,c("Id","winPlacePerc")],file="submission.csv",row.names = FALSE)
 
