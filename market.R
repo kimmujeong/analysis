9134347e558a7054c09ca604180cf4e58b66b611
kimmujeong <anwjd7@gmail.com>
2019-03-29 16:19:18 +0900

    0329

--- market.R
+++ market.R
@@ -3,14 +3,24 @@
 library(dplyr)
 library(ggplot2)
 library(arules) #연관분석
+library(Matrix)
+library(arulesViz)
+library(RColorBrewer)
+library(splitstackshape)
 #install.packages("arules")
-
+#install.packages("tidyverse")
+#install.packages("arulesViz")
+#install.packages("RColorBrewer")
+install.packages("splitstackshape")
 aisles<-read.csv("C:\\Users\\thgus\\Downloads\\instacart-market-basket-analysis\\aisles.csv")
 departments<-read.csv("C:\\Users\\thgus\\Downloads\\instacart-market-basket-analysis\\departments.csv")
 order_products__prior<-read.csv("C:\\Users\\thgus\\Downloads\\instacart-market-basket-analysis\\order_products__prior.csv")
 order_products__train<-read.csv("C:\\Users\\thgus\\Downloads\\instacart-market-basket-analysis\\order_products__train.csv")
 orders<-read.csv("C:\\Users\\thgus\\Downloads\\instacart-market-basket-analysis\\orders.csv")
 products<-read.csv("C:\\Users\\thgus\\Downloads\\instacart-market-basket-analysis\\products.csv")
+
+
+mydata<-read.csv("C:\\Users\\thgus\\Downloads\\instacart-market-basket-analysis\\order_products__prior.csv",nrow=20000)
 
 rm(list=ls())
 
@@ -55,7 +65,7 @@ head(order_products__train[order(order_products__train$product_id),])
 order_products__train %>%
   group_by(product_id) %>%
   summarise(prob=mean(reordered),n=n()) %>% #n추가
-  filter(n>40) %>% #40횟 이상 
+  filter(n>40) %>% #40회 이상 
   arrange(desc(prob)) %>%
   left_join(products,by="product_id")
 ####################################################
@@ -77,21 +87,63 @@ orders %>%
 
 
 #알고리즘
-#order_products__train에서 재구매여부 연관성 아이템 찾아보기
-#데이터 형태를 바꿔야할것같음 희소행렬로..
-head(order_products__train,20)
-#데이터변환
-order_baskets<-order_products__train %>% 
-  inner_join(products, by="product_id") %>% 
-  group_by(order_id) %>%
-  summarise(basket = as.vector(list(product_name)))
+#train파일로 하면 규칙이 안나온다.. prior파일로 실행했음..
+sample<-order_products__prior[1:20000,]
+sample_join<-sample %>%
+  left_join(products,by="product_id")
+#sample_join  
+sample_join_split<-split(sample_join$product_name, sample_join$order_id)
+sample_join_transactions<-as(sample_join_split,"transactions")
+rules<-apriori(sample_join_transactions, parameter=list(support=0.001, confidence=0.8, minlen = 3))
+summary(rules)
+inspect(rules[1:50])
 
-# test<-data.frame()
+inspect(subset(rules, subset = rhs %pin% "Banana")[1:10])
+lift_top5<-head(sort(rules, by ="lift"),5) #연관성이 강한 5개 뽑기
+inspect(lift_top5)
+plot(lift_top5, method = "graph",control=list(type="items",main="")) #plot 찍을 때는 inspect 없는걸로 해야됨
+
+
+#######kaggle
+rules<-apriori(sample_join_split, parameter=list(supp=0.001,conf = 0.8), 
+               appearance = list(default="lhs",rhs="Banana"),
+               control = list(verbose=F))
+rules<-sort(rules, decreasing=TRUE,by="confidence")
+inspect(rules[1:10])
+plot(rules,control=list(col=brewer.pal(11,"Spectral")),main="")
+subrules2 <- head(sort(rules, by="lift"), 10)
+plot(subrules2, method="graph",control=list(type="items",main=""))
+
+
+#####kaggle 연습
+rules_test<-apriori(sample_join_transactions, parameter=list(support=0.001, confidence=0.8),appearance = list(default="lhs",rhs="Banana"),control = list(verbose=F))
+rules_test<-sort(rules_test, decreasing=TRUE,by="confidence")
+plot_data<-head(sort(rules_test, by ="lift"),5)
+plot(plot_data, method = "graph",control=list(type="items",main=""))
+
+
+###순차분석
+
+
+
+# test<-data.frame(matrix(nrow=131209,ncol=39123))
 # row<-distinct(order_products__train,order_id) #중복값 제거
 # col<-distinct(order_products__train,product_id) #중복값 제거
 
+# order_baskets$product_name<-as.vector(list(order_baskets$product_name))
+# transactions <- as(order_baskets$product_name, "transactions")
+# inspect(transactions[1])
 
-# order_products__train_list<-split(order_products__train$order_id,order_products__train$product_id,order_products__train$add_to_cart_order,order_products__train$reordered)
-# order_products__train_trans<-as(order_products__train_list,"transactions")
-# rules <- apriori(data = order_products__train_trans, parameter = list(support = 0.01, confidence = 0.8, minlen = 1))
-# summary(rules)
+
+
+########################## kaggle
+# head(mydata)
+# mydata<-mydata[,1:2] #picking up first two rows
+# mydata<-merge(mydata,products,by="product_id") #merging
+# mydata<-arrange(mydata, order_id) # ascending order
+# mydata<-mydata[,c(2,3)] #dropping other columns
+# mydata[1:10,] # sneak peek
+# dt <- split(mydata$product_name, mydata$order_id)
+# dt2 = as(dt,"transactions")
+# rules = apriori(dt2, parameter=list(support=0.001, confidence=0.8, minlen = 3))
+# rules
