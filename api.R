b33b6fe9bad62e19f0f028ccb13094ac3baccd6e
kimmujeong <anwjd7@gmail.com>
2019-04-18 16:53:38 +0900

    0418

--- api.R
+++ api.R
@@ -5,13 +5,13 @@ library(dplyr)
 api_url<-"http://apis.data.go.kr/B552061/frequentzoneLg/getRestFrequentzoneLg"
 servicekey<-"oxrnAfkJN4PXQnHw7cNJANXNzXxIz6B6ebL8oWffyvb9nb5mqpQX4ANZBzFnUmhG2XhKl9PQ8TDQ7u5xY974WQ%3D%3D"
 '
-강북구:305 
-종로구:110 
-관악구:620 
-은평구:380 
-강동구:740 
-금천구:545 
-서초구:650 
+강북구:305
+종로구:110
+관악구:620
+은평구:380
+강동구:740
+금천구:545
+서초구:650
 서대문구:410
 '
 url<-paste0(api_url,"?","serviceKey=",servicekey,"&searchYearCd=",2017,"&siDo=",11,"&guGun=",305,"&type=","xml","&numOfRows=",10,"&pageNo=",1)
@@ -40,17 +40,109 @@ for (i in 1:row_length){
 
 location_200
 location_200<-lapply(location_200,as.double) #문자형태를 숫자형태로 변환 
-location_200<-data.frame(location_200$V1,location_200$V2) #다시 데이터 프레임 형태로 
-location_200<-setNames(data.frame(location_200),c("lon","lat")) #dplyr 이용해서 rename(t1,"lon"=V1,"lat"=V2)가능
+location_200<-data.frame(location_200$V1,location_200$V2) #다시 데이터 프레임 형태로 , v인지 x인지 확인해야 함 
+location_200<-setNames(data.frame(location_200),c("lon","lat")) #dplyr 이용해서 rename(,"lon"=V1,"lat"=V2)가능
 
-################
-location_200_1<-location_200
-location_200_2<-location_200
-location_200_3<-location_200
 
-get_googlemap("sungdonggu", maptype="roadmap", zoom=13) %>%
-  ggmap+geom_point(data=location_200,aes(x=lon, y = lat), color='red')
+###############################################################################
+###########################for문 처리 완료#####################################
+url<-paste0(api_url,"?","serviceKey=",servicekey,"&searchYearCd=",2017,"&siDo=",11,"&guGun=",305,"&type=","xml","&numOfRows=",10,"&pageNo=",1)
+raw.data <- xmlTreeParse(url, useInternalNodes = TRUE, encoding = "utf-8")
+rootNode <- xmlRoot(raw.data)
+items <- rootNode[[2]][['items']]
 
-head(location_200_1)
-head(location_200_2)
-head(location_200_3)
+size<-xmlSize(items)
+item<-list()
+loc<-list()
+
+for (i in 1:size){
+  item_temp <- xmlSApply(items[[i]],xmlValue)
+  item_temp <- item_temp['geom_json']
+  item[[i]] <- item_temp
+  
+  item[[i]]<-substr(item[[i]],33+2,nchar(item[[i]])-3)
+  item[[i]]<-str_extract_all(item[[i]],'(?<=\\[)[0-9]+[.?0-9]+,[0-9]+[.0-9]+')
+  row_length=length(item[[i]][[1]])
+  location_df<-data.frame(matrix(nrow=row_length, ncol=2))
+  for (a in 1:row_length){
+    for (b in 1:2){
+      tmp<-str_split(item[[i]][[1]][a],',')
+      location_df[a,b]<-tmp[[1]][b]
+    }
+  }
+  location_df<-lapply(location_df,as.double) #문자형태를 숫자형태로 변환 
+  location_df<-data.frame(location_df$X1,location_df$X2) #다시 데이터 프레임 형태로 
+  location_df<-rename(location_df,"lon"=location_df.X1,"lat"=location_df.X2)
+  loc[[i]]<-location_df
+}
+###############################################################################
+
+
+##################function 테스트 힌트...##############################
+test_function(1)  
+test_function("gangbuk") #function으로 만들어서 loc->"입력 스트링"
+########################################################################
+
+########################################################################
+#######################function만들기##################################
+many_accident_function<-function(x){
+  if(x=="gangbuk") #switch문 switch('gangbuk','gangbuk'=code<-20)
+    code<-305
+  else if(x=="jongno")
+    code<-110
+  else if(x=="gwanak")
+    code<-620
+  else if(x=="eunpyeong")
+    code<-380
+  else if(x=="gangdong")
+    code<-740
+  else if(x=="geumcheon")
+    code<-545
+  else if(x=="seocho")
+    code<-650
+  else if(x=="seodaemun")
+    code<-410
+    
+  url<-paste0(api_url,"?","serviceKey=",servicekey,"&searchYearCd=",2017,"&siDo=",11,"&guGun=",code,"&type=","xml","&numOfRows=",10,"&pageNo=",1)
+  raw.data <- xmlTreeParse(url, useInternalNodes = TRUE, encoding = "utf-8")
+  rootNode <- xmlRoot(raw.data)
+  items <- rootNode[[2]][['items']]
+  
+  size<-xmlSize(items)
+  item<-list()
+  x<-list()
+  
+  for (i in 1:size){
+    item_temp <- xmlSApply(items[[i]],xmlValue)
+    item_temp <- item_temp['geom_json']
+    item[[i]] <- item_temp
+    
+    item[[i]]<-substr(item[[i]],33+2,nchar(item[[i]])-3)
+    item[[i]]<-str_extract_all(item[[i]],'(?<=\\[)[0-9]+[.?0-9]+,[0-9]+[.0-9]+')
+    row_length=length(item[[i]][[1]])
+    location_df<-data.frame(matrix(nrow=row_length, ncol=2))
+    for (a in 1:row_length){
+      for (b in 1:2){
+        tmp<-str_split(item[[i]][[1]][a],',')
+        location_df[a,b]<-tmp[[1]][b]
+      }
+    }
+    location_df<-lapply(location_df,as.double) #문자형태를 숫자형태로 변환 
+    location_df<-data.frame(location_df$X1,location_df$X2) #다시 데이터 프레임 형태로 
+    location_df<-rename(location_df,"lon"=location_df.X1,"lat"=location_df.X2)
+    x[[i]]<-location_df
+  }
+  return(x)
+}
+########################################################################
+
+ma_gangbuk_loc<-many_accident_function("gangbuk")
+ma_jongno_loc<-many_accident_function("jongno")
+ma_gwanak_loc<-many_accident_function("gwanak")
+ma_eunpyeong_loc<-many_accident_function("eunpyeong")
+ma_gangdong_loc<-many_accident_function("gangdong")
+ma_geumcheon_loc<-many_accident_function("geumcheon")
+ma_seocho_loc<-many_accident_function("seocho")
+ma_seodaemun_loc<-many_accident_function("seodaemun")
+
+
--- traffic.R
+++ traffic.R
@@ -75,43 +75,84 @@ get_googlemap("seoul", maptype="roadmap", zoom=11) %>%
   geom_point(data=geumcheon,aes(x=lon,y=lat),color='pink')+
   geom_point(data=seocho,aes(x=lon,y=lat),color='green')+
   geom_point(data=seodaemun,aes(x=lon,y=lat),color='violet')+
-  geom_point(data=location_200,aes(x=lon, y = lat), color='red')
+  geom_point(data=ma_gangbuk_loc[[1]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_gangbuk_loc[[2]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_gangbuk_loc[[3]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_jongno_loc[[1]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_jongno_loc[[2]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_jongno_loc[[3]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_gwanak_loc[[1]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_gwanak_loc[[2]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_gwanak_loc[[3]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_eunpyeong_loc[[1]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_eunpyeong_loc[[2]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_eunpyeong_loc[[3]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_gangdong_loc[[1]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_gangdong_loc[[2]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_gangdong_loc[[3]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_geumcheon_loc[[1]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_geumcheon_loc[[2]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_geumcheon_loc[[3]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_seocho_loc[[1]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_seocho_loc[[2]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_seocho_loc[[3]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_seodaemun_loc[[1]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_seodaemun_loc[[2]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_seodaemun_loc[[3]],aes(x=lon, y = lat), color='red')
 
 #강북구
 get_googlemap("gangbukgu", maptype="roadmap", zoom=13) %>%
   ggmap+geom_point(data=gangbuk, aes(x=lon,y=lat))+
-  geom_point(data=location_200_1,aes(x=lon, y = lat), color='red')+
-  geom_point(data=location_200_2,aes(x=lon, y = lat), color='red')+
-  geom_point(data=location_200_3,aes(x=lon, y = lat), color='red')
+  geom_point(data=ma_gangbuk_loc[[1]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_gangbuk_loc[[2]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_gangbuk_loc[[3]],aes(x=lon, y = lat), color='red')
 
 #종로구
 get_googlemap("jongnogu", maptype="roadmap", zoom=13) %>%
-  ggmap+geom_point(data=jongno, aes(x=lon,y=lat))
+  ggmap+geom_point(data=jongno, aes(x=lon,y=lat))+
+  geom_point(data=ma_jongno_loc[[1]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_jongno_loc[[2]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_jongno_loc[[3]],aes(x=lon, y = lat), color='red')
 
 #관악구
 get_googlemap("gwanakgu", maptype="roadmap", zoom=13) %>%
-  ggmap+geom_point(data=gwanak, aes(x=lon,y=lat))
+  ggmap+geom_point(data=gwanak, aes(x=lon,y=lat))+
+  geom_point(data=ma_gwanak_loc[[1]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_gwanak_loc[[2]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_gwanak_loc[[3]],aes(x=lon, y = lat), color='red')
 
 #은평구
 get_googlemap("eunpyeonggu", maptype="roadmap", zoom=13) %>%
-  ggmap+geom_point(data=eunpyeong, aes(x=lon,y=lat))
+  ggmap+geom_point(data=eunpyeong, aes(x=lon,y=lat))+
+  geom_point(data=ma_eunpyeong_loc[[1]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_eunpyeong_loc[[2]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_eunpyeong_loc[[3]],aes(x=lon, y = lat), color='red')
 
 #강동구
 get_googlemap("gangdonggu", maptype="roadmap", zoom=13) %>%
-  ggmap+geom_point(data=gangdong, aes(x=lon,y=lat))
+  ggmap+geom_point(data=gangdong, aes(x=lon,y=lat))+
+  geom_point(data=ma_gangdong_loc[[1]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_gangdong_loc[[2]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_gangdong_loc[[3]],aes(x=lon, y = lat), color='red')
 
 #금천구
 get_googlemap("geumcheongu", maptype="roadmap", zoom=13) %>%
-  ggmap+geom_point(data=geumcheon, aes(x=lon,y=lat))
+  ggmap+geom_point(data=geumcheon, aes(x=lon,y=lat))+
+  geom_point(data=ma_geumcheon_loc[[1]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_geumcheon_loc[[2]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_geumcheon_loc[[3]],aes(x=lon, y = lat), color='red')
 
 #서초구
 get_googlemap("seochogu", maptype="roadmap", zoom=13) %>%
-  ggmap+geom_point(data=seocho, aes(x=lon,y=lat))
+  ggmap+geom_point(data=seocho, aes(x=lon,y=lat))+
+  geom_point(data=ma_seocho_loc[[1]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_seocho_loc[[2]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_seocho_loc[[3]],aes(x=lon, y = lat), color='red')
 
 #서대문구
 get_googlemap("seodaemungu", maptype="roadmap", zoom=13) %>%
-  ggmap+geom_point(data=seodaemun, aes(x=lon,y=lat))
+  ggmap+geom_point(data=seodaemun, aes(x=lon,y=lat))+
+  geom_point(data=ma_seodaemun_loc[[1]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_seodaemun_loc[[2]],aes(x=lon, y = lat), color='red')+
+  geom_point(data=ma_seodaemun_loc[[3]],aes(x=lon, y = lat), color='red')
 ###################################################################
-
-location_200
-seodaemun
