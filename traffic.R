c494c883f337859a13e78d614068766c59086031
kimmujeong <anwjd7@gmail.com>
2019-04-23 19:33:46 +0900

    0423

--- traffic.R
+++ traffic.R
@@ -103,25 +103,25 @@ get_googlemap("seoul", maptype="roadmap", zoom=11) %>%
 #강북구
 get_googlemap("gangbukgu", maptype="roadmap", zoom=13) %>%
   ggmap+geom_point(data=gangbuk, aes(x=lon,y=lat))+
-  geom_point(data=ma_gangbuk_loc[[1]],aes(x=lon, y = lat), color='red')+ #사고다발지역
-  geom_point(data=ma_gangbuk_loc[[2]],aes(x=lon, y = lat), color='red')+
-  geom_point(data=ma_gangbuk_loc[[3]],aes(x=lon, y = lat), color='red')+
-  geom_point(data=bike_gangbuk_loc[[1]],aes(x=lon, y=lat), color='orange')+ #자전거
-  geom_point(data=bike_gangbuk_loc[[2]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=bike_gangbuk_loc[[3]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=bike_gangbuk_loc[[4]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=bike_gangbuk_loc[[5]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=bike_gangbuk_loc[[6]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=bike_gangbuk_loc[[7]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=bike_gangbuk_loc[[8]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=bike_gangbuk_loc[[9]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=older_gangbuk_loc[[1]],aes(x=lon,y=lat),color='blue')+ # 노인보행사고
-  geom_point(data=older_gangbuk_loc[[2]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_gangbuk_loc[[3]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_gangbuk_loc[[4]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_gangbuk_loc[[5]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_gangbuk_loc[[6]],aes(x=lon,y=lat),color='blue')
-  # geom_point(data=death_gangbuk_loc,aes(x=경도, y=위도),color="deep pink")
+  # geom_point(data=ma_gangbuk_loc[[1]],aes(x=lon, y = lat), color='red')+ #사고다발지역
+  # geom_point(data=ma_gangbuk_loc[[2]],aes(x=lon, y = lat), color='red')+
+  # geom_point(data=ma_gangbuk_loc[[3]],aes(x=lon, y = lat), color='red')+
+  # geom_point(data=bike_gangbuk_loc[[1]],aes(x=lon, y=lat), color='orange')+ #자전거
+  # geom_point(data=bike_gangbuk_loc[[2]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=bike_gangbuk_loc[[3]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=bike_gangbuk_loc[[4]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=bike_gangbuk_loc[[5]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=bike_gangbuk_loc[[6]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=bike_gangbuk_loc[[7]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=bike_gangbuk_loc[[8]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=bike_gangbuk_loc[[9]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=older_gangbuk_loc[[1]],aes(x=lon,y=lat),color='blue')+ # 노인보행사고
+  # geom_point(data=older_gangbuk_loc[[2]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_gangbuk_loc[[3]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_gangbuk_loc[[4]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_gangbuk_loc[[5]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_gangbuk_loc[[6]],aes(x=lon,y=lat),color='blue')+
+  geom_point(data=death_gangbuk_loc,aes(x=경도, y=위도),fill="red",shape=25,size=3) #사망교통사고
 
 #종로구
 get_googlemap("jongnogu", maptype="roadmap", zoom=13) %>%
@@ -139,102 +139,102 @@ get_googlemap("jongnogu", maptype="roadmap", zoom=13) %>%
   geom_point(data=older_jongno_loc[[5]],aes(x=lon,y=lat),color='blue')+
   geom_point(data=older_jongno_loc[[6]],aes(x=lon,y=lat),color='blue')+
   geom_point(data=older_jongno_loc[[7]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=death_jongno_loc,aes(x=경도, y=위도),color="deep pink")
+  geom_point(data=death_jongno_loc,aes(x=경도, y=위도),fill="red",shape=25,size=3)
 
 #관악구
 get_googlemap("gwanakgu", maptype="roadmap", zoom=13) %>%
   ggmap+geom_point(data=gwanak, aes(x=lon,y=lat))+
-  geom_point(data=ma_gwanak_loc[[1]],aes(x=lon, y = lat), color='red')+
-  geom_point(data=ma_gwanak_loc[[2]],aes(x=lon, y = lat), color='red')+
-  geom_point(data=ma_gwanak_loc[[3]],aes(x=lon, y = lat), color='red')+
-  geom_point(data=bike_gwanak_loc[[1]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=bike_gwanak_loc[[2]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=older_gwanak_loc[[1]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_gwanak_loc[[2]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_gwanak_loc[[3]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_gwanak_loc[[4]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_gwanak_loc[[5]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_gwanak_loc[[6]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_gwanak_loc[[7]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=death_gwanak_loc,aes(x=경도, y=위도),color="deep pink")
+  # geom_point(data=ma_gwanak_loc[[1]],aes(x=lon, y = lat), color='red')+
+  # geom_point(data=ma_gwanak_loc[[2]],aes(x=lon, y = lat), color='red')+
+  # geom_point(data=ma_gwanak_loc[[3]],aes(x=lon, y = lat), color='red')+
+  # geom_point(data=bike_gwanak_loc[[1]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=bike_gwanak_loc[[2]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=older_gwanak_loc[[1]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_gwanak_loc[[2]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_gwanak_loc[[3]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_gwanak_loc[[4]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_gwanak_loc[[5]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_gwanak_loc[[6]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_gwanak_loc[[7]],aes(x=lon,y=lat),color='blue')+
+  geom_point(data=death_gwanak_loc,aes(x=경도, y=위도),fill="red",shape=25,size=3)
 
 #은평구
 get_googlemap("eunpyeonggu", maptype="roadmap", zoom=13) %>%
   ggmap+geom_point(data=eunpyeong, aes(x=lon,y=lat))+
-  geom_point(data=ma_eunpyeong_loc[[1]],aes(x=lon, y = lat), color='red')+
-  geom_point(data=ma_eunpyeong_loc[[2]],aes(x=lon, y = lat), color='red')+
-  geom_point(data=ma_eunpyeong_loc[[3]],aes(x=lon, y = lat), color='red')+
-  geom_point(data=bike_eunpyeong_loc[[1]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=bike_eunpyeong_loc[[2]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=bike_eunpyeong_loc[[3]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=older_eunpyeong_loc[[1]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_eunpyeong_loc[[2]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_eunpyeong_loc[[3]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_eunpyeong_loc[[4]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_eunpyeong_loc[[5]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_eunpyeong_loc[[6]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_eunpyeong_loc[[7]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=death_eunpyeong_loc,aes(x=경도, y=위도),color="deep pink")
+  # geom_point(data=ma_eunpyeong_loc[[1]],aes(x=lon, y = lat), color='red')+
+  # geom_point(data=ma_eunpyeong_loc[[2]],aes(x=lon, y = lat), color='red')+
+  # geom_point(data=ma_eunpyeong_loc[[3]],aes(x=lon, y = lat), color='red')+
+  # geom_point(data=bike_eunpyeong_loc[[1]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=bike_eunpyeong_loc[[2]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=bike_eunpyeong_loc[[3]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=older_eunpyeong_loc[[1]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_eunpyeong_loc[[2]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_eunpyeong_loc[[3]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_eunpyeong_loc[[4]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_eunpyeong_loc[[5]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_eunpyeong_loc[[6]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_eunpyeong_loc[[7]],aes(x=lon,y=lat),color='blue')+
+  geom_point(data=death_eunpyeong_loc,aes(x=경도, y=위도),fill="red",shape=25,size=3)
 
 #강동구
 get_googlemap("gangdonggu", maptype="roadmap", zoom=13) %>%
   ggmap+geom_point(data=gangdong, aes(x=lon,y=lat))+
-  geom_point(data=ma_gangdong_loc[[1]],aes(x=lon, y = lat), color='red')+
-  geom_point(data=ma_gangdong_loc[[2]],aes(x=lon, y = lat), color='red')+
-  geom_point(data=ma_gangdong_loc[[3]],aes(x=lon, y = lat), color='red')+
-  geom_point(data=bike_gangdong_loc[[1]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=bike_gangdong_loc[[2]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=bike_gangdong_loc[[3]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=bike_gangdong_loc[[4]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=bike_gangdong_loc[[5]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=bike_gangdong_loc[[6]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=bike_gangdong_loc[[7]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=older_gangdong_loc[[1]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_gangdong_loc[[2]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_gangdong_loc[[3]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_gangdong_loc[[4]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_gangdong_loc[[5]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_gangdong_loc[[6]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_gangdong_loc[[7]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=death_gangdong_loc,aes(x=경도, y=위도),color="deep pink")
+  # geom_point(data=ma_gangdong_loc[[1]],aes(x=lon, y = lat), color='red')+
+  # geom_point(data=ma_gangdong_loc[[2]],aes(x=lon, y = lat), color='red')+
+  # geom_point(data=ma_gangdong_loc[[3]],aes(x=lon, y = lat), color='red')+
+  # geom_point(data=bike_gangdong_loc[[1]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=bike_gangdong_loc[[2]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=bike_gangdong_loc[[3]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=bike_gangdong_loc[[4]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=bike_gangdong_loc[[5]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=bike_gangdong_loc[[6]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=bike_gangdong_loc[[7]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=older_gangdong_loc[[1]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_gangdong_loc[[2]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_gangdong_loc[[3]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_gangdong_loc[[4]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_gangdong_loc[[5]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_gangdong_loc[[6]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_gangdong_loc[[7]],aes(x=lon,y=lat),color='blue')+
+  geom_point(data=death_gangdong_loc,aes(x=경도, y=위도),fill="red",shape=25,size=3)
 
 #금천구
 get_googlemap("geumcheongu", maptype="roadmap", zoom=13) %>%
   ggmap+geom_point(data=geumcheon, aes(x=lon,y=lat))+
-  geom_point(data=ma_geumcheon_loc[[1]],aes(x=lon, y = lat), color='red')+
-  geom_point(data=ma_geumcheon_loc[[2]],aes(x=lon, y = lat), color='red')+
-  geom_point(data=ma_geumcheon_loc[[3]],aes(x=lon, y = lat), color='red')+
-  geom_point(data=bike_geumcheon_loc[[1]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=bike_geumcheon_loc[[2]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=bike_geumcheon_loc[[3]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=older_geumcheon_loc[[1]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_geumcheon_loc[[2]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_geumcheon_loc[[3]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=death_geumcheon_loc,aes(x=경도, y=위도),color="deep pink")
+  # geom_point(data=ma_geumcheon_loc[[1]],aes(x=lon, y = lat), color='red')+
+  # geom_point(data=ma_geumcheon_loc[[2]],aes(x=lon, y = lat), color='red')+
+  # geom_point(data=ma_geumcheon_loc[[3]],aes(x=lon, y = lat), color='red')+
+  # geom_point(data=bike_geumcheon_loc[[1]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=bike_geumcheon_loc[[2]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=bike_geumcheon_loc[[3]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=older_geumcheon_loc[[1]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_geumcheon_loc[[2]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_geumcheon_loc[[3]],aes(x=lon,y=lat),color='blue')+
+  geom_point(data=death_geumcheon_loc,aes(x=경도, y=위도),fill="red",shape=25,size=3)
 
 #서초구 #자전거데이터없음 
 get_googlemap("seochogu", maptype="roadmap", zoom=13) %>%
   ggmap+geom_point(data=seocho, aes(x=lon,y=lat))+
-  geom_point(data=ma_seocho_loc[[1]],aes(x=lon, y = lat), color='red')+
-  geom_point(data=ma_seocho_loc[[2]],aes(x=lon, y = lat), color='red')+
-  geom_point(data=ma_seocho_loc[[3]],aes(x=lon, y = lat), color='red')+
-  geom_point(data=older_seocho_loc[[1]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_seocho_loc[[2]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=death_seocho_loc,aes(x=경도, y=위도),color="deep pink")
+  # geom_point(data=ma_seocho_loc[[1]],aes(x=lon, y = lat), color='red')+
+  # geom_point(data=ma_seocho_loc[[2]],aes(x=lon, y = lat), color='red')+
+  # geom_point(data=ma_seocho_loc[[3]],aes(x=lon, y = lat), color='red')+
+  # geom_point(data=older_seocho_loc[[1]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_seocho_loc[[2]],aes(x=lon,y=lat),color='blue')+
+  geom_point(data=death_seocho_loc,aes(x=경도, y=위도),fill="red",shape=25,size=3)
 
 #서대문구
 get_googlemap("seodaemungu", maptype="roadmap", zoom=13) %>%
   ggmap+geom_point(data=seodaemun, aes(x=lon,y=lat))+
-  geom_point(data=ma_seodaemun_loc[[1]],aes(x=lon, y = lat), color='red')+
-  geom_point(data=ma_seodaemun_loc[[2]],aes(x=lon, y = lat), color='red')+
-  geom_point(data=ma_seodaemun_loc[[3]],aes(x=lon, y = lat), color='red')+
-  geom_point(data=bike_seodaemun_loc[[1]],aes(x=lon, y=lat), color='orange')+
-  geom_point(data=older_seodaemun_loc[[1]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_seodaemun_loc[[2]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_seodaemun_loc[[3]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_seodaemun_loc[[4]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_seodaemun_loc[[5]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_seodaemun_loc[[6]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=older_seodaemun_loc[[7]],aes(x=lon,y=lat),color='blue')+
-  geom_point(data=death_seodaemun_loc,aes(x=경도, y=위도),color="deep pink")
+  # geom_point(data=ma_seodaemun_loc[[1]],aes(x=lon, y = lat), color='red')+
+  # geom_point(data=ma_seodaemun_loc[[2]],aes(x=lon, y = lat), color='red')+
+  # geom_point(data=ma_seodaemun_loc[[3]],aes(x=lon, y = lat), color='red')+
+  # geom_point(data=bike_seodaemun_loc[[1]],aes(x=lon, y=lat), color='orange')+
+  # geom_point(data=older_seodaemun_loc[[1]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_seodaemun_loc[[2]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_seodaemun_loc[[3]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_seodaemun_loc[[4]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_seodaemun_loc[[5]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_seodaemun_loc[[6]],aes(x=lon,y=lat),color='blue')+
+  # geom_point(data=older_seodaemun_loc[[7]],aes(x=lon,y=lat),color='blue')+
+  geom_point(data=death_seodaemun_loc,aes(x=경도, y=위도),fill="red",shape=25,size=3)
 ###################################################################
